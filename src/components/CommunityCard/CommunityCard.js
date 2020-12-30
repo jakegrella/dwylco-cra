c03882244baab8c89974ab766b8e509e0e4889c1
Jake Grella <jgrella13@gmail.com>
2020-12-30 12:07:30 -0600

    links for com posts

--- src/components/CommunityCard/CommunityCard.js
+++ src/components/CommunityCard/CommunityCard.js
@@ -6,12 +6,14 @@ export default function ProductCard(props) {
 	const { title, author, date, imgSrc } = props;
 	return (
 		<StyledCommunityCard>
-			<img src={imgSrc} alt='' />
-			<h3>{title}</h3>
-			<p>
-				By {author} - {date}
-			</p>
-			{/* <p>description</p> */}
+			<Link to='/community'>
+				<img src={imgSrc} alt='' />
+				<h3>{title}</h3>
+				<p>
+					By {author} - {date}
+				</p>
+				{/* <p>description</p> */}
+			</Link>
 		</StyledCommunityCard>
 	);
 }
